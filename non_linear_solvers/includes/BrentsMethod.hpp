590cdf823573243c6fad0d7a6fec1e0d0e383cec
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2025-03-18 13:49:09 +0000

    adding solver using the Brent's method

---
+++ non_linear_solvers/includes/BrentsMethod.hpp
@@ -0,0 +1,15 @@
+#ifndef BRENTS_METHOD_HPP
+#define BRENTS_METHOD_HPP
+#include "Function.hpp"
+
+#include <cstddef>
+
+double secanteCalculation(const Function &, double, double);
+
+double inverseQuadraticInterpolation(const Function &, double, double, double);
+
+bool useBisection(double, double, double, double, double, double, bool);
+
+double BrentsMethod(const Function &, double, double, double, size_t);
+
+#endif // BRENTS_METHOD_HPP
---
+++ non_linear_solvers/src/BrentsMethod.cpp
@@ -0,0 +1,97 @@
+#include "BrentsMethod.hpp"
+#include <cassert>
+#include <cmath>
+
+double secanteCalculation(const Function &myFunction, double a, double b) {
+  return b - myFunction.Value(b) * (b - a) /
+                 (myFunction.Value(b) - myFunction.Value(a));
+}
+
+double inverseQuadraticInterpolation(const Function &myFunction, double a,
+                                     double b_0, double b_1) {
+
+  double fa = myFunction.Value(a);
+  double fb_0 = myFunction.Value(b_0);
+  double fb_1 = myFunction.Value(b_1);
+
+  double P1 = a * fb_0 * fb_1 / ((fa - fb_0) * (fa - fb_1));
+  double P2 = b_1 * fa * fb_0 / ((fb_1 - fa) * (fb_1 - fb_0));
+  double P3 = b_0 * fa * fb_1 / ((fb_0 - fa) * (fb_0 - fb_1));
+
+  return P1 + P2 + P3;
+}
+
+bool useBisection(double a, double b_0, double b_1, double b_2, double s,
+                  double epsilon, bool bisection) {
+
+  double delta = epsilon + std::numeric_limits<double>::min();
+
+  bool P1 = bisection && (std::abs(s - b_2) >= 0.5 * std::abs(b_2 - b_1));
+  bool P2 = !bisection && (std::abs(s - b_2) >= 0.5 * std::abs(b_1 - b_0));
+  bool P3 = bisection && (std::abs(b_2 - b_1) < delta);
+  bool P4 = !bisection && (std::abs(b_1 - b_0) < delta);
+
+  return P1 || P2 || P3 || P4;
+}
+
+double BrentsMethod(const Function &myFunction, double a, double b,
+                    double epsilon, size_t maxIterations) {
+
+  double product = myFunction.Value(a) * myFunction.Value(b);
+
+  assert(product < 0.);
+  if (myFunction.Value(a) < myFunction.Value(b))
+    std::swap(a, b);
+
+  size_t k = 1;
+  double a_k = a;
+  double b_0 = a, b_1 = a, b_2 = b;
+  double s = std::numeric_limits<double>::max();
+  double fs = std::numeric_limits<double>::max();
+
+  bool bisection = true;
+
+  while ((std::abs(myFunction.Value(b_1)) > epsilon) &&
+         std::abs(fs) > epsilon && std::abs(b_2 - a_k) > epsilon &&
+         (k <= maxIterations)) {
+
+    bool inverse_quadratic_interpolation_condition =
+        (myFunction.Value(a_k) != myFunction.Value(b_1) &&
+         myFunction.Value(b_2) != myFunction.Value(b_1));
+
+    if (inverse_quadratic_interpolation_condition) {
+      s = inverseQuadraticInterpolation(myFunction, a_k, b_1, b_2);
+    } else {
+      s = secanteCalculation(myFunction, a_k, b_2);
+    }
+
+    bool bisection_condition =
+        useBisection(a_k, b_0, b_1, b_2, s, epsilon, bisection);
+
+    if (bisection_condition) {
+      s = a_k + 0.5 * (b_2 - a_k);
+      bisection = true;
+    } else {
+      bisection = false;
+    }
+
+    fs = myFunction.Value(s);
+    b_0 = b_1;
+    b_1 = b_2;
+
+    product = myFunction.Value(a_k) * fs;
+    if (product < 0.)
+      b_2 = s;
+    else
+      a_k = s;
+
+    product = myFunction.Value(a_k) * myFunction.Value(b_2);
+    assert(product < 0.);
+    if (myFunction.Value(a_k) < myFunction.Value(b_2))
+      std::swap(a_k, b_2);
+  }
+
+  double fb = myFunction.Value(b_2);
+
+  return fb < fs ? b : s;
+}
