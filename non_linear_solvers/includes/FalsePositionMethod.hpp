412dbd7052fca55d4ef168a8aab548ed3c4188f5
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2025-04-05 13:19:43 +0100

    keep refactoring

--- non_linear_solvers/includes/FalsePositionMethod.hpp
+++ non_linear_solvers/includes/FalsePositionMethod.hpp
@@ -4,7 +4,7 @@
 #include "Function.hpp"
 #include <cstddef>
 
-double FalsePositionMethod(const Function &, double, double, double = 0.0001,
+double FalsePositionMethod(const FunctionC0 &, double, double, double = 0.0001,
                            size_t = 100);
 
 #endif // FALSE_POSITION_METHOD_HPP
--- non_linear_solvers/src/FalsePositionMethod.cpp
+++ non_linear_solvers/src/FalsePositionMethod.cpp
@@ -1,39 +1,46 @@
 #include "FalsePositionMethod.hpp"
+#include "Function.hpp"
 #include <cassert>
 #include <cmath>
 
-double FalsePositionMethod(const Function& myFunction, double a, double b, double epsilon, size_t maxIterations){
+double FalsePositionMethod(const FunctionC0 &myFunction, double a, double b,
+                           double epsilon, size_t maxIterations) {
 
-    assert(a<b);
+  assert(a < b);
 
-    size_t k = 1;
-    double a_k = a, b_k = b;
-    double r_k = a_k;
+  size_t k = 1;
+  double a_k = a, b_k = b;
+  double r_k = a_k;
 
-    double diff_k = myFunction.Value(b_k) - myFunction.Value(a_k);
+  double diff_k = myFunction(b_k) - myFunction(a_k);
 
-    assert( std::abs(diff_k) > epsilon);
-    double inv_diff_k = 1./diff_k;
-    
+  assert(std::abs(diff_k) > epsilon);
+  double inv_diff_k = 1. / diff_k;
 
-    while( (std::abs(myFunction.Value(r_k))>epsilon) && (k<=maxIterations)){
+  double f_rk = myFunction(r_k);
+  double f_ak = myFunction(a_k);
+  double f_bk = myFunction(b_k);
+  double product = 0.;
 
-        r_k = a_k - myFunction.Value(a_k)*(b_k - a_k)*inv_diff_k;
-        double product = myFunction.Value(a_k)*myFunction.Value(r_k);
+  while ((std::abs(f_rk) > epsilon) && (k <= maxIterations)) {
 
-        if (product > 0.){
-            a_k = r_k;        
-        }
-        else{
-            b_k = r_k;
-        }
+    r_k = a_k - f_ak * (b_k - a_k) * inv_diff_k;
+    f_rk = myFunction(r_k);
+    product = f_ak * f_rk;
 
-        diff_k = myFunction.Value(b_k) - myFunction.Value(a_k);
-        assert( std::abs(diff_k) > epsilon);
-        inv_diff_k = 1./diff_k;
-        k++;
+    if (product > 0.) {
+      a_k = r_k;
+      f_ak = myFunction(a_k);
+    } else {
+      b_k = r_k;
+      f_bk = myFunction(b_k);
     }
 
-    return r_k;
+    diff_k = f_bk - f_ak;
+    assert(std::abs(diff_k) > epsilon);
+    inv_diff_k = 1. / diff_k;
+    k++;
+  }
 
+  return r_k;
 }
